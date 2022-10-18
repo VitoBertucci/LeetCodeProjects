35ea335ac1da26d3a70629851b1d133cba78c9b2
VitoBertucci <vito.rob.bertucci@gmail.com>
2022-10-18 14:45:41 -0400

    leetcode 104 solved

---
+++ leet104.java
@@ -0,0 +1,17 @@
+import Classes.TreeNode;
+public class leet104 {
+
+    TreeNode root;
+    
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+        return 0;
+ 
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+ 
+        int bigger = Math.max(leftDepth, rightDepth);
+ 
+        return bigger+1;
+    }
+}
